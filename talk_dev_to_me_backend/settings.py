c4d5db5c3f5a013952e600c9a05fdbe17ba41478
Ashley Casimir <ashcas@me.com>
2021-05-15 14:56:27 -0700

    i didint change anything

--- talk_dev_to_me_backend/settings.py
+++ talk_dev_to_me_backend/settings.py
@@ -36,6 +36,10 @@ ALLOWED_HOSTS = tuple(env.list("ALLOWED_HOSTS"))
 # Application definition
 
 INSTALLED_APPS = [
+<<<<<<< HEAD
+=======
+
+>>>>>>> de35ebaa2ea3071a42d7bbe50aeb56a49f6db5c7
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
     "backend",
     "rest_framework",
     "corsheaders",
+<<<<<<< HEAD
 ]
 
 MIDDLEWARE = [
@@ -57,6 +62,43 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+=======
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'backend',
+    'rest_framework',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+>>>>>>> de35ebaa2ea3071a42d7bbe50aeb56a49f6db5c7
 ]
 
 ROOT_URLCONF = "talk_dev_to_me_backend.urls"
@@ -151,3 +193,7 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+<<<<<<< HEAD
+=======
+
+>>>>>>> de35ebaa2ea3071a42d7bbe50aeb56a49f6db5c7
